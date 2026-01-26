ef8644363ef2818b87b39d9dfc6036c13be3fe80
Daniella Bertoldi <bertoldi.daniella@gmail.com>
2026-01-25 22:58:04 -0500

    added wee 2 folder, installled python, git add . git commit -m "Exercise 2.3 - Python quote program" git push

---
+++ week-2/quote.py
@@ -0,0 +1,12 @@
+"""
+Author: Daniella Bertoldi
+Date: January 25, 2026
+File Name: quote.py
+Description: This program prints a quote that connects science and technology
+and credits the original author.
+"""
+
+# This program outputs a quote related to science and technology
+
+print("“The science of today is the technology of tomorrow.”")
+print("— Edward Teller")
